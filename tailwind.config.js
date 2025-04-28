29e4a6f7b6b119a09c0eb4e71255b54c18161e1b
hungvannguyen <nguyenvanhung08012002@gmail.com>
2025-04-28 10:38:19 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./views/**/*.html", './index.html',],
+  theme: {
+    extend: {
+      colors: {
+        'dark': '#161616',
+      },
+      fontSize: {
+        xs: ['0.75rem', '1.25'],
+        sm: ['0.875rem', '1.25'],
+        base: ['1rem', '1.25'],
+        lg: ['1.125rem', '1.25'],
+        xl: ['1.25rem', '1.25'],
+        '2xl': ['1.45rem', '1.25'],
+        '3xl': ['1.875rem', '1.25'],
+        '4xl': ['2.25rem', '1.25'],
+        '5xl': ['3rem', '1.25'],
+        '6xl': ['3.57rem', '1.25'],
+      }
+    }
+  },
+  plugins: [],
+}
+
